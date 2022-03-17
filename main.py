13e0b16b94463961ac6ab4c247ab9f8a6d61fd8f
zlymac <1147497022@qq.com>
2022-03-17 21:07:31 +0800

    移除ssl验证

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ import urllib3
 from bs4 import BeautifulSoup
 
 
-def main(laravel_session): #参数为cookie里的laravel_session 自行抓包获取
+def main(laravel_session):  # 参数为cookie里的laravel_session 自行抓包获取
     s = requests.session()  # 创建会话
     loginurl = "https://service.jiangsugqt.org/youth/lesson"  # 江苏省青年大学习接口
-
+    # 参数
+    params = {
+        "s": "/youth/lesson",
+        "form": "inglemessage",
+        "isappinstalled": "0"
+    }
     # 构造请求头
     headers = {
         # 'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x18001234) NetType/WIFI Language/zh_CN",
-        'Cookie': "laravel_session="+laravel_session # 抓包获取
-            # "laravel_session=6bkiNtcb7Nhbe73AYoODf90H5xpUfdDMScNtFF4F"
+        'Cookie': "laravel_session=" + laravel_session  # 抓包获取
+        # "laravel_session=6bkiNtcb7Nhbe73AYoODf90H5xpUfdDMScNtFF4F"
         # 'Cookie':"8rAucTd84mpMLxilmCjeWO08rbtC7opDnrwo9YvJ"
-        # 8rAucTd84mpMLxilmCjeWO08rbtC7opDnrwo9YvJ 周良宇 003831928
+        # 8rAucTd84mpMLxilmCjeWO08rbtC7opDnrwo9YvJ
+        # 74FrRKCDVZKhx91w0a4CDG53DmkeXCxBOkSzTTNH周良宇 003831928
         # esX66JF8QROB5yx89KMpFBwnF2eNrVUbSpx8FVUX 姜宇 008629871
         # vf6yckniFRDqepGNSaSD2SN4IhCv1wj6LPPqfh74 李靖翔
     }
-    urllib3.disable_warnings() #不然会有warning
-    login = s.get(url=loginurl, headers=headers,verify=False)  # 登录
+    urllib3.disable_warnings()  # 不然会有warning
+    login = s.get(url=loginurl, headers=headers, params=params, verify=False)  # 登录
+
     # print(login.text)
     login_soup = BeautifulSoup(login.text, 'html.parser')  # 解析信息确认页面
     # print(soup.select(".confirm-user-info"))
@@ -43,7 +50,6 @@ def main(laravel_session): #参数为cookie里的laravel_session 自行抓包获
     dict['token'] = token[0]
     dict['lesson_id'] = lesson_id[0]
 
-
     print(dict)
     confirmurl = "https://service.jiangsugqt.org/youth/lesson/confirm"
     params = {
@@ -61,5 +67,5 @@ def main(laravel_session): #参数为cookie里的laravel_session 自行抓包获
 
 
 if __name__ == '__main__':
-    laravel_session="74FrRKCDVZKhx91w0a4CDG53DmkeXCxBOkSzTTNH"
+    laravel_session = "8rAucTd84mpMLxilmCjeWO08rbtC7opDnrwo9YvJ"
     main(laravel_session)
